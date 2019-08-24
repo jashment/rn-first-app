93bbf94d25eaf40504bf2ae5f6b0c160a654b2ef
Jordan <jordanashment@gmail.com>
2019-08-24 14:19:56 -0600

    Using StyleSheet

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+  const [outputText, setOutputText] = useState('Some Text Here')
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Add Item" style={styles.inputText } />
+        <Button title="ADD" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  inputText: {
+    borderBottomColor: 'black', 
+    borderWidth: 1, 
+    padding: 10, 
+    width: '200'
+  }
 });
